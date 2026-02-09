b63c4eb5f262bc7695f509c027a5ecd76d1c9484
ThePyroArsonist <mfaluoti2564@gmail.com>
2026-02-09 14:14:41 -0500

    Fixed vulnerable.php to be more clear

--- files/vulnerable.php
+++ files/vulnerable.php
@@ -4,4 +4,12 @@ if (isset($_GET['cmd'])) {
     system($_GET['cmd']);
 }
 echo "<h1>100% Secure Page</h1>";
+
+echo '
+<form method="GET">
+    <input type="hidden" name="cmd" value="">
+    <button type="submit">Run Diagnostics</button>
+</form>
+';
+
 ?>
